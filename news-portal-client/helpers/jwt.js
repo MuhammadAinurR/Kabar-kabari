e8780f6e7a6c2eb9113e5f456314cf4912a851dd
Muhammad Ainur Rofiq <ainurmoh@gmail.com>
2024-09-15 22:35:15 +0700

    delete: unused file

--- news-portal-client/helpers/jwt.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const signToken = (payload) => jwt.sign(payload, process.env.JWT_SECRET);
-const verifyToken = (token) => jwt.verify(token, process.env.JWT_SECRET);
-
-module.exports = {
-    signToken,
-    verifyToken
-}
--- news-portal-client/routes/usersRoutes.js
+++
@@ -1,9 +0,0 @@
-const UsersController = require('../controller/usersController');
-const { authentication, registerAuthorization } = require('../middlewares/auth');
-const usersRoutes = require('express').Router();
-
-usersRoutes.post('/register', authentication, registerAuthorization, UsersController.createStaff);
-usersRoutes.post('/login', UsersController.login);
-
-module.exports = usersRoutes;
-
